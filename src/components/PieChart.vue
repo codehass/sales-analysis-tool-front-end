b693ea28c9141ee2b104aa3c6418cf70848c0e10
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2024-12-06 18:17:18 +0100

    create pi chart component

---
+++ src/components/PieChart.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+import { defineProps, ref, onMounted } from 'vue';
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement,
+} from 'chart.js';
+import { Pie } from 'vue-chartjs';
+import type { ChartData, ChartOptions } from 'chart.js';
+
+// Register required Chart.js components
+ChartJS.register(Title, Tooltip, Legend, ArcElement);
+
+// Define props with proper types
+defineProps({
+  chartData: {
+    type: Object as () => ChartData<'pie', number[], string>,
+    required: true,
+  },
+  chartOptions: {
+    type: Object as () => ChartOptions<'pie'>,
+    required: false,
+  },
+});
+
+const chartRef = ref();
+onMounted(() => {
+  if (chartRef.value) {
+    chartRef.value.render();
+  }
+});
+</script>
+
+<template>
+  <Pie :data="chartData" :options="chartOptions" ref="chartRef" />
+</template>
