1e5422a788d36088ba2267839f78bff0bcfd59c6
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2024-12-07 19:50:12 +0100

    install tailwindcss

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import DashboardView from '@/views/DashboardView.vue';
 import ProductsView from '@/views/ProductsView.vue';
+import '@/assets/tailwind.css';
 
 const routes = [
   { path: '/',
